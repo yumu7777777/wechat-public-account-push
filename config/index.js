d9ab93d9930177cd9982f211be87c2290f09525c
yumu <104643678+yumu7777777@users.noreply.github.com>
2022-08-28 19:42:46 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ export const config = {
     // 公众号APP_SECRET
     APP_SECRET: "43e28ef2489258b83b4f0e8bf003ec36",
     // 模板消息id
-    TEMPLATE_ID: "rCCT5RKD8sz31lsn7Kr58wzRcohSBPYqRdnWX4qnvsw",
+    TEMPLATE_ID: "08FXJylyj_ETCL56pRK2I8Om857d5Sw5w0HDc-XkYvY",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     USERS: ["oWjzm5lYCS0qu9yXq5Q0xKSU7k_c","oWjzm5lj9W7o5QsYBqk4tEq7GMNc"],
      
